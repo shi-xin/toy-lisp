089cecfd6c0cb6615198484ec93cce3e984251d8
Shi Xin <stevexinshi@gmail.com>
2020-01-08 14:01:48 +0800

    parsing basic lisp like math operations

--- prompt.c
+++ prompt.c
@@ -26,9 +26,74 @@ it, replies back with some message. This system is called a REPL.
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "mpc.h"  /* file is local, use "" */
+
 #include <editline/readline.h>
 
+
+/* use operator string to decide which operation to perform */
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+
+/* evaluation function */
+/* ok, now this is recursion, and it is unlike the way the author had described
+it */
+long eval(mpc_ast_t* t) {
+    /* if tagged as number return it directly */
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    /* the operator is always second child */
+    char* op = t->children[1]->contents;
+
+    /* we store the third child in 'x' */
+    long x = eval(t->children[2]);
+
+    /* iterate the remaining children and combining. */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 int main(int argc, char** argv) {
+
+    /* create some parsers */
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+
+    /* define them with the following language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                        \
+            number    : /-?[0-9]+/ ;                             \
+            operator  : '+' | '-' | '*' | '/' ;                  \
+            expr      : <number> | '(' <operator> <expr>+ ')' ;  \
+            lispy     : /^/ <operator> <expr>+ /$/ ;             \
+        ",
+        Number, Operator, Expr, Lispy);
     /* print version and exit information */
     puts("Toy Lisp v0.1");
     puts("Press control+c to Exit\n");
@@ -38,16 +103,28 @@ int main(int argc, char** argv) {
 
         /* output prompt and get input */
         char* input = readline("toy-lisp>>> ");
-
         /* add input to history */
         add_history(input);
 
+
         /* echo input back to user */
-        printf(": %s\n", input);
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            
+            long result = eval(r.output);
+            printf("%li\n", result);
+            mpc_ast_delete(r.output);
+        } else {
+            /* otherwise print the error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         /* free retrieved input */
         free(input);
     }
+    /* undefine and delete our parsers */
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
